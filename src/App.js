5882fbf2da78668b7b0cf9343089e9dbb09385f1
mr-vikash <mrvikash700@gmail.com>
2023-08-26 23:27:23 +0530

    learn-usereducer-hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Header from './components/Header';
+import { useReducer } from 'react';
+
+const App =()=> {
+  const reducer =(state,action)=>
+  {
+     if(action.type==="inc")
+     {
+      return state = state + 2;
+     }
+     else if(action.type==="dec")
+     {
+      return state=state-2;
+     }
+     else if(action.type==="mul")
+     {
+      return state = state*2;
+     }
+     else if(action.type ==="div")
+     {
+      return state = state/2;
+     }
+     return state;
+  } 
+  const [state,dispatch] = useReducer(reducer,0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <h2>Press button for doing mathematical operation by 2</h2>
+      <h1>{state}</h1>
+      <button onClick={()=>dispatch({type:"inc"})}>Add</button>
+      <button onClick={()=>dispatch({type:"dec"})}>Dec</button>
+      <button onClick={()=>dispatch({type:"mul"})}>Mul</button>
+      <button onClick={()=>dispatch({type:"div"})}>Div</button>
     </div>
   );
 }
